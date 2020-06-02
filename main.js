e3ccd3068727c06cbbf1b80f55a8a8365ed7ee5a
shna rafeeq <shnar.majeed@gmail.com>
2020-06-02 12:56:24 +0000

    Automatically backed up by Learn

--- main.js
+++ main.js
@@ -21,8 +21,7 @@ function handleClick(e){
     if(lisHeart.innerHTML == `${EMPTY_HEART}`){
      lisHeart.innerHTML= `${FULL_HEART}`;
      lisHeart.classList.add('activated-heart');
-    }
-    else{
+    }else{
      lisHeart.innerHTML= `${EMPTY_HEART}`;
      lisHeart.classList.remove('activated-heart');
     }
