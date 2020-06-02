7a92555f7dcea26993196dda5b8ee1f36ae42e93
shna rafeeq <shnar.majeed@gmail.com>
2020-06-02 13:22:36 +0000

    Done.

--- main.js
+++ main.js
@@ -3,7 +3,40 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+let lisHeart = document.getElementsByClassName('like-glyph');
+let errorDiv = document.getElementById('modal');
+let modalMessage = document.getElementById('modal-message');
 
+for(let li of lisHeart) {
+   li.addEventListener('click',handleClick);
+}
+
+function handleClick(e){
+  const ele = e.target;
+ 
+  mimicServerCall()
+  .then(function(){
+    
+    if(ele.innerHTML == `${EMPTY_HEART}`){
+     ele.innerHTML= `${FULL_HEART}`;
+     ele.classList.add('activated-heart');
+    }
+    else{
+     ele.innerHTML= `${EMPTY_HEART}`;
+     ele.classList.remove('activated-heart');
+    }
+    
+  })
+  .catch(function(e){
+    console.log(e)
+    errorDiv.classList.remove('hidden');
+    modalMessage.innerText = "Random server error. Try again.";
+  })
+  setTimeout(hideError,5000);
+}
+function hideError(){
+  errorDiv.classList.add('hidden');
+ }
 
 
 
